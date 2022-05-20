243661755502a5e8764ec6502b9f11026ca2a867
Sachin S R <sogoduraju@google.com>
2022-05-19 23:53:31 -0700

    New: Enabled cors

--- edu-analytics-be/server.js
+++ edu-analytics-be/server.js
@@ -20,6 +20,12 @@ app.use(cors());
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true })); */
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.get('/', (req, res) => {
     res.send(`Server is Ok`);
