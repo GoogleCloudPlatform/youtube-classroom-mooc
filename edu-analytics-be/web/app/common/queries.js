9f6012e5fa6b54b3e1292113ae6a7384b32f8ab8
Jon Chen <jonchen@google.com>
2022-09-06 17:25:59 -0400

    add license header to all source files

--- edu-analytics-be/web/app/common/queries.js
+++ edu-analytics-be/web/app/common/queries.js
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 module.exports = Object.freeze({
     CREATE_TABLES: 'CREATE TABLE if not exists students (studentId int NOT NULL AUTO_INCREMENT,email varchar(255) NOT NULL,firstName varchar(255) NOT NULL,lastName varchar(255) NOT NULL,stdId varchar(255),createdDate DATETIME DEFAULT NOW(),PRIMARY KEY (studentId),UNIQUE (email));'+
                     'CREATE TABLE if not exists educators (educatorId int NOT NULL AUTO_INCREMENT,email varchar(255) NOT NULL,firstName varchar(255) NOT NULL,lastName varchar(255) NOT NULL,eduId varchar(255),createdDate DATETIME DEFAULT NOW(),PRIMARY KEY (educatorId),UNIQUE (email));'+
